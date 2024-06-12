Basic unified script done. Need to fix m_u gradient issue and try to train models in parallel.

mamba/ops/triton/k_activations.py
@@ -1,7 +1,7 @@
 # =============================================================================#
 # Authors: Tri Dao, Albert Gu, Windsor Nguyen
 # File: selective_state_update.py
-# Adapted from 
+# Adapted from
 # https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/triton/k_activations.py
 # =============================================================================#
 
@@ -41,8 +41,8 @@ def _swiglu_fwd_kernel(
     Y += row * stride_y_row
     OUT += row * stride_out_row
     cols = start_col + tl.arange(0, BLOCK_N)
-    x = tl.load(X + cols, mask=cols < ncols, other=0.).to(tl.float32)
-    y = tl.load(Y + cols, mask=cols < ncols, other=0.).to(tl.float32)
+    x = tl.load(X + cols, mask=cols < ncols, other=0.0).to(tl.float32)
+    y = tl.load(Y + cols, mask=cols < ncols, other=0.0).to(tl.float32)
     out = x * tl.sigmoid(x) * y
     tl.store(OUT + cols, out, mask=cols < ncols)
 
@@ -62,7 +62,9 @@ def _swiglu_fwd(xy, out=None):
     M, N = x.shape
     grid = lambda META: (M, triton.cdiv(N, META['BLOCK_N']))
     with torch.cuda.device(x.device.index):
-        _swiglu_fwd_kernel[grid](x, y, out, x.stride(0), y.stride(0), out.stride(0), N)
+        _swiglu_fwd_kernel[grid](
+            x, y, out, x.stride(0), y.stride(0), out.stride(0), N
+        )
     return out.reshape(*batch_shape, out.shape[-1])
 
 
@@ -77,7 +79,7 @@ def _swiglu_fwd(xy, out=None):
     ],
     key=['ncols'],
 )
-@triton.heuristics({"RECOMPUTE_OUTPUT": lambda args: args["OUT"] is not None})
+@triton.heuristics({'RECOMPUTE_OUTPUT': lambda args: args['OUT'] is not None})
 @triton.jit
 def _swiglu_bwd_kernel(
     X,
@@ -107,9 +109,9 @@ def _swiglu_bwd_kernel(
     DX += row * stride_dx_row
     DY += row * stride_dy_row
     cols = start_col + tl.arange(0, BLOCK_N)
-    x = tl.load(X + cols, mask=cols < ncols, other=0.).to(tl.float32)
-    y = tl.load(Y + cols, mask=cols < ncols, other=0.).to(tl.float32)
-    dout = tl.load(DOUT + cols, mask=cols < ncols, other=0.).to(tl.float32)
+    x = tl.load(X + cols, mask=cols < ncols, other=0.0).to(tl.float32)
+    y = tl.load(Y + cols, mask=cols < ncols, other=0.0).to(tl.float32)
+    dout = tl.load(DOUT + cols, mask=cols < ncols, other=0.0).to(tl.float32)
     x_sigmoid = tl.sigmoid(x)
     dx = x_sigmoid * (1 + x * (1 - x_sigmoid)) * y * dout
     dy = x * x_sigmoid * dout
@@ -148,12 +150,24 @@ def _swiglu_bwd(xy, dout, dxy=None, recompute_output=False, out=None):
     M, N = x.shape
     grid = lambda META: (M, triton.cdiv(N, META['BLOCK_N']))
     with torch.cuda.device(x.device.index):
-        _swiglu_bwd_kernel[grid](x, y, dout, out if recompute_output else None, dx, dy,
-                                 x.stride(0), y.stride(0), dout.stride(0),
-                                 out.stride(0) if recompute_output else 0,
-                                 dx.stride(0), dy.stride(0),
-                                 N)
+        _swiglu_bwd_kernel[grid](
+            x,
+            y,
+            dout,
+            out if recompute_output else None,
+            dx,
+            dy,
+            x.stride(0),
+            y.stride(0),
+            dout.stride(0),
+            out.stride(0) if recompute_output else 0,
+            dx.stride(0),
+            dy.stride(0),
+            N,
+        )
     if not recompute_output:
         return dxy.reshape(*batch_shape, dxy.shape[-1])
     else:
-        return dxy.reshape(*batch_shape, dxy.shape[-1]), out.reshape(*batch_shape, out.shape[-1])
+        return dxy.reshape(*batch_shape, dxy.shape[-1]), out.reshape(
+            *batch_shape, out.shape[-1]
+        )

stu/model.py
@@ -41,7 +41,7 @@ class STU(nn.Module):
 
     def __init__(
         self,
-        d_out: int = 37,
+        d_out: int = 24,
         input_len: int = 1000,
         num_eigh: int = 24,
         auto_reg_k_u: int = 3,
@@ -80,38 +80,37 @@ class STU(nn.Module):
         self.m_phi = nn.Parameter(torch.zeros(self.d_out * self.k, self.d_out))
 
     def apply_stu(self, inputs):
-        start_time = time.time()  # Start timing
+        # start_time = time.time()  # Start timing
         eig_vals, eig_vecs = self.eigh
 
         x_tilde = stu_utils.compute_x_tilde(inputs, (eig_vals, eig_vecs))
-        print(f'Time for x_tilde computation: {time.time() - start_time:.4f}s')
-        start_time = time.time()  # Reset timing
+        # print(f'Time for x_tilde computation: {time.time() - start_time:.4f}s')
+        # start_time = time.time()  # Reset timing
 
-        # delta_phi = x_tilde @ self.m_phi
-        delta_phi = torch.einsum('lk,kd->ld', x_tilde, self.m_phi)
-        print(
-            f'Time for delta_phi computation: {time.time() - start_time:.4f}s'
-        )
-        start_time = time.time()  # Reset timing
+        delta_phi = x_tilde @ self.m_phi
+        # print(
+        #     f'Time for delta_phi computation: {time.time() - start_time:.4f}s'
+        # )
+        # start_time = time.time()  # Reset timing
 
         delta_ar_u = stu_utils.compute_ar_x_preds(self.m_u, inputs)
-        print(
-            f'Time for delta_ar_u computation: {time.time() - start_time:.4f}s'
-        )
-        start_time = time.time()  # Reset timing
+        # print(
+        #     f'Time for delta_ar_u computation: {time.time() - start_time:.4f}s'
+        # )
+        # start_time = time.time()  # Reset timing
 
         y_t = stu_utils.compute_y_t(self.m_y, delta_phi + delta_ar_u)
-        print(f'Time for y_t computation: {time.time() - start_time:.4f}s')
+        # print(f'Time for y_t computation: {time.time() - start_time:.4f}s')
 
         return y_t
 
     # TODO: Remove vmap and handle batch sizes manually
     def forward(self, inputs):
-        start_time = time.time()
+        # start_time = time.time()
         output = torch.vmap(self.apply_stu)(inputs)
-        print(
-            f'Total time for STU forward pass: {time.time() - start_time:.4f}s'
-        )
+        # print(
+        #     f'Total time for STU forward pass: {time.time() - start_time:.4f}s'
+        # )
         return output
 
 
@@ -155,28 +154,28 @@ class Architecture(nn.Module):
         self.projection = nn.Linear(self.d_model, self.d_target)
 
     def forward(self, inputs):
-        start_time = time.time()  # Start timing for the embedding operation
+        # start_time = time.time()  # Start timing for the embedding operation
         x = self.embedding(inputs)
-        embedding_time = time.time() - start_time
-        print(f'Time for embedding: {embedding_time:.4f}s')
+        # embedding_time = time.time() - start_time
+        # print(f'Time for embedding: {embedding_time:.4f}s')
 
-        total_layer_time = 0
+        # total_layer_time = 0
 
         for i, layer in enumerate(self.layers):
-            start_time = time.time()  # Start timing for each layer
+            # start_time = time.time()  # Start timing for each layer
             z = x
             x = layer(x)
             x = x + z
-            layer_time = time.time() - start_time
-            total_layer_time += layer_time
-            print(f'Time for layer {i}: {layer_time:.4f}s')
+            # layer_time = time.time() - start_time
+            # total_layer_time += layer_time
+            # print(f'Time for layer {i}: {layer_time:.4f}s')
 
-        print(f'Total time for all layers: {total_layer_time:.4f}s')
+        # print(f'Total time for all layers: {total_layer_time:.4f}s')
 
-        start_time = time.time()  # Start timing for the final projection
+        # start_time = time.time()  # Start timing for the final projection
         output = self.projection(x)
-        projection_time = time.time() - start_time
-        print(f'Time for final projection: {projection_time:.4f}s')
+        # projection_time = time.time() - start_time
+        # print(f'Time for final projection: {projection_time:.4f}s')
         return output
 
     def predict(

train.py
@@ -13,7 +13,10 @@ from socket import gethostname
 
 import matplotlib.pyplot as plt
 import numpy as np
+import safetensors
+from safetensors.torch import save_file
 import torch
+import torch.nn.functional as F
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 from tqdm import tqdm
@@ -21,12 +24,28 @@ from tqdm import tqdm
 from losses.loss_ant import AntLoss
 from losses.loss_cheetah import HalfCheetahLoss
 from losses.loss_walker import Walker2DLoss
-from stu import model, experiment as exp, optimizer as opt
-from stu.model import STUConfigs
+from stu import experiment as exp, optimizer as opt
+from stu.model import STUConfigs, Architecture
 from stu.physics import physics_data
 from transformer.model import Transformer, TransformerConfigs
 # from mamba.model import Mamba, MambaConfig
 # from jamba.model import Jamba, JambaConfig
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def colored_print(text, color):
+    print(f'{color}{text}{Colors.ENDC}')
 
 
 def set_seed(seed: int) -> None:
@@ -79,6 +98,10 @@ def setup(
     return device, local_rank, world_size
 
 
+def cleanup():
+    dist.destroy_process_group()
+
+
 def gaussian_kernel(size, sigma):
     """Creates a 1D Gaussian kernel using PyTorch."""
     size = int(size) // 2
@@ -96,10 +119,10 @@ def smooth_curve(points, sigma=2):
     points = torch.tensor(points, dtype=torch.float32)
     kernel = gaussian_kernel(kernel_size, sigma).unsqueeze(0)
     # Apply padding to handle borders
-    points_padded = torch.nn.functional.pad(
+    points_padded = F.pad(
         points, (kernel_size // 2, kernel_size // 2), mode='reflect'
     )
-    smoothed_points = torch.nn.functional.conv1d(
+    smoothed_points = F.conv1d(
         points_padded.unsqueeze(0).unsqueeze(0), kernel.unsqueeze(0)
     )
     return smoothed_points.squeeze().numpy()
@@ -193,8 +216,10 @@ def main() -> None:
     val_batch_size: int = (
         16 // world_size
     )  # scale batch size for distributed training
+    # num_epochs: int = 3
+    # eval_period: int = 30
     num_epochs: int = 3
-    eval_period: int = 30
+    eval_period: int = num_epochs * 10
     patience: int = 5
     checkpoint_dir: str = 'checkpoints'
 
@@ -206,7 +231,7 @@ def main() -> None:
     # STU hyperparameters
     d_model: int = 24
     d_target: int = 18
-    num_layers: int = 6
+    num_layers: int = 1
     dropout: float = 0.25
     input_len: int = 1000
     num_eigh: int = 24
@@ -216,14 +241,15 @@ def main() -> None:
     stu_lr: float = 7.5e-4
 
     # Transformer hyperparameters
-    n_layer: int = 6
-    n_head: int = 1
-    n_embd: int = 37
+    n_layer: int = 1
+    n_head: int = 8
+    n_embd: int = 24
     scale: int = 4
-    d_out: int = 29
+    d_out: int = 18
     max_len: int = 1_000
     bias: bool = False  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     dropout: float = 0.25
+    use_dilated_attn: bool = False
     transformer_lr: float = 7.5e-4
 
     # Mamba hyperparameters
@@ -270,15 +296,14 @@ def main() -> None:
 
     models = {}
     experiments = {}
-    loss_fn = (
-        HalfCheetahLoss()
-        if controller == 'HalfCheetah-v1'
-        else Walker2DLoss()
-        if controller == 'Walker2D-v1'
-        else AntLoss()
-    )
+    loss_fn = {
+        'HalfCheetah-v1': HalfCheetahLoss,
+        'Walker2D-v1': Walker2DLoss,
+        'Ant-v1': AntLoss,
+    }[controller]()
 
     # Define the models based on flags
+    print(num_layers)
     if 'stu' in args.models:
         stu_configs = STUConfigs(
             d_model=d_model,
@@ -292,7 +317,7 @@ def main() -> None:
             learnable_m_y=learnable_m_y,
             loss_fn=loss_fn,
         )
-        stu_model = model.Architecture(stu_configs).to(device)
+        stu_model = Architecture(stu_configs).to(device)
 
         if world_size > 1:
             stu_model = DDP(
@@ -332,6 +357,7 @@ def main() -> None:
             max_len=max_len,
             bias=bias,
             dropout=dropout,
+            use_dilated_attn=use_dilated_attn,
             loss_fn=loss_fn,
         )
         transformer_model = Transformer(transformer_configs).to(device)
@@ -448,9 +474,13 @@ def main() -> None:
     }
 
     if main_process:
-        models = get_models(args.models)
         grmr = 'models' if len(args.models) > 1 else 'model'
-        msg = f"Lyla: We'll be training the {models} {grmr} on the {controller} task with"
+        models_str = (
+            ', '.join(args.models[:-1]) + f', and {args.models[-1]}'
+            if len(args.models) > 1
+            else args.models[0]
+        )
+        msg = f"Lyla: We'll be training the {models_str} {grmr} on the {controller} task with"
         if world_size > 1:
             print(
                 f'{msg} {device} on rank {rank + 1}/{world_size}'
@@ -459,29 +489,37 @@ def main() -> None:
         else:
             print(f'{msg} {device} today.')
 
-    # Prepare for training
-    pbar = (
-        tqdm(
-            range(num_epochs * len(train_loader)), desc='Training', unit='step'
+    # Prepare progress bars for training
+    pbars = {
+        model_name: tqdm(
+            range(num_epochs * len(train_loader)),
+            desc=f'Training {model_name}',
+            unit='step',
+            position=i,
         )
-        if main_process
-        else range(num_epochs * len(train_loader))
-    )
+        for i, model_name in enumerate(models)
+    }
+
+    # TODO: Check that setting this to True doesn't break distributed training.
     torch.autograd.set_detect_anomaly(True)
 
     # Training loop!
     for epoch in range(num_epochs):
         for step, (inputs, targets) in enumerate(train_loader):
-            for model_name in args.models:
-                model = models[model_name]
+            for model_name in models:
                 experiment = experiments[model_name]
+                train_metrics = experiment.step(inputs, targets)
 
-                if model_name == 'transformer':
-                    outputs, loss = model(inputs, targets)
-                    train_metrics = experiment.step(loss)
-                else:
-                    outputs = model(inputs)
-                    train_metrics = experiment.step(outputs, targets)
+                if dist.is_initialized():
+                    # Gather metrics from all processes
+                    gathered_metrics = [None] * world_size
+                    dist.all_gather_object(gathered_metrics, train_metrics)
+
+                    # Aggregate metrics across all processes
+                    train_metrics = {
+                        k: sum(d[k] for d in gathered_metrics) / world_size
+                        for k in train_metrics.keys()
+                    }
 
                 # Append the losses and metrics for each model
                 train_losses[model_name].append(train_metrics['loss'])
@@ -512,47 +550,46 @@ def main() -> None:
                             postfix_dict[f'{model_name}_{metric}'] = (
                                 train_metrics[metric]
                             )
-                    pbar.set_postfix(postfix_dict)
+                    pbars[model_name].set_postfix(postfix_dict)
 
-                if main_process:
-                    pbar.update(1)
+            if main_process:
+                for model_name in models:
+                    pbars[model_name].update(1)
 
             total_steps = epoch * len(train_loader) + step
+
+            if total_steps > 0 and total_steps % 10 == 0:
+                if main_process:
+                    colored_print(f'\nStep: {total_steps}', Colors.BOLD)
+                    for model_name in models:
+                        colored_print(
+                            f'{model_name} - Train Loss: {train_losses[model_name][-1]:.4f}',
+                            Colors.OKBLUE,
+                        )
 
             if total_steps > 0 and total_steps % eval_period == 0:
                 for model_name, experiment in experiments.items():
                     val_metrics = experiment.evaluate(val_loader)
+
+                    if dist.is_initialized():
+                        # Gather validation metrics from all processes
+                        gathered_metrics = [None] * world_size
+                        dist.all_gather_object(gathered_metrics, val_metrics)
+
+                        # Aggregate validation metrics across all processes
+                        val_metrics = {
+                            k: sum(d[k] for d in gathered_metrics) / world_size
+                            for k in val_metrics.keys()
+                        }
+
                     val_losses[model_name].append(val_metrics['loss'])
 
-                    if world_size > 1:
-                        # Gather evaluation metrics from all processes
-                        gathered_metrics = [None] * world_size
-                        torch.distributed.all_gather_object(
-                            gathered_metrics, val_metrics
+                    if main_process:
+                        colored_print(
+                            f'\nLyla: Evaluating the {model_name} model on step {total_steps} Loss: {val_metrics["loss"]:.2f}.',
+                            Colors.OKCYAN,
                         )
 
-                        if main_process:
-                            # Aggregate metrics across all processes
-                            total_loss = (
-                                sum(
-                                    metric['loss']
-                                    for metric in gathered_metrics
-                                )
-                                / world_size
-                            )
-                            print(
-                                f'\nLyla: Evaluating the {model_name} model on step {total_steps}'
-                                f' Average Loss: {total_loss:.4f}.'
-                            )
-                            val_metrics = {'loss': total_loss}
-                    else:
-                        if main_process:
-                            print(
-                                f'\nLyla: Evaluating the {model_name} model on step {total_steps}'
-                                f' Loss: {val_metrics["loss"]:.2f}.'
-                            )
-
-                    if main_process:
                         val_loss = val_metrics['loss']
                         if val_loss < best_val_losses[model_name]:
                             best_val_losses[model_name] = val_loss
@@ -565,32 +602,48 @@ def main() -> None:
                             )
                             best_checkpoints[model_name] = checkpoint_filename
 
-                            torch.save(
-                                models[model_name].state_dict(), checkpoint_path
-                            )
-                            print(
-                                f'Lyla: Wow! We have a new personal best for the {model_name} model at step {total_steps}.'
-                                f' The validation loss improved to: {val_loss:.4f}!'
-                                f' Checkpoint saved as {checkpoint_path}'
+                            if dist.is_initialized():
+                                # Save the model on the main process and broadcast it to all processes
+                                if main_process:
+                                    save_file(
+                                        models[model_name].module.state_dict(),
+                                        checkpoint_path,
+                                    )
+                                dist.barrier()
+                                map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
+                                models[model_name].load_state_dict(
+                                    safetensors.load_file(
+                                        checkpoint_path, device=map_location
+                                    )
+                                )
+                            else:
+                                save_file(
+                                    models[model_name].state_dict(),
+                                    checkpoint_path,
+                                )
+
+                            colored_print(
+                                f'Lyla: Wow! We have a new personal best for the {model_name} model at step {total_steps}. The validation loss improved to: {val_loss:.4f}! Checkpoint saved as {checkpoint_path}',
+                                Colors.OKGREEN,
                             )
                         else:
                             patient_counters[model_name] += 1
-                            print(
-                                f'Lyla: No improvement in validation loss for the {model_name} model'
-                                f' for {patient_counters[model_name]} eval periods.'
-                                f' Current best loss: {best_val_losses[model_name]:.4f}.'
+                            colored_print(
+                                f'Lyla: No improvement in validation loss for the {model_name} model for {patient_counters[model_name]} eval periods. Current best loss: {best_val_losses[model_name]:.4f}.',
+                                Colors.WARNING,
                             )
 
-                        if patient_counters[model_name] >= patience:
-                            print(
-                                f'Lyla: We have reached the patience limit of {patience}'
-                                f' for the {model_name} model. Stopping the training early'
-                                f' at step {total_steps}...'
-                            )
-                            dist.barrier()
-                            return
+                            if patient_counters[model_name] >= patience:
+                                colored_print(
+                                    f'Lyla: We have reached the patience limit of {patience} for the {model_name} model. Stopping the training early at step {total_steps}...',
+                                    Colors.FAIL,
+                                )
+                                if dist.is_initialized():
+                                    dist.barrier()
+                                return
 
-    pbar.close()
+    for pbar in pbars.values():
+        pbar.close()
 
     if main_process:
         print('\nLyla: Training completed!')
@@ -598,7 +651,25 @@ def main() -> None:
             best_checkpoint_path = os.path.join(
                 checkpoint_dir, best_checkpoints[model_name]
             )
-            models[model_name].load_state_dict(torch.load(best_checkpoint_path))
+
+            if dist.is_initialized():
+                # Load the best checkpoint on the main process and broadcast it to all processes
+                if main_process:
+                    models[model_name].load_state_dict(
+                        safetensors.load_file(best_checkpoint_path)
+                    )
+                dist.barrier()
+                map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
+                models[model_name].load_state_dict(
+                    safetensors.load_file(
+                        best_checkpoint_path, device=map_location
+                    )
+                )
+            else:
+                models[model_name].load_state_dict(
+                    safetensors.load_file(best_checkpoint_path)
+                )
+
             print(
                 f"\nLyla: Here's the best model information for the {model_name} model:"
             )
@@ -623,8 +694,7 @@ def main() -> None:
                     f'Best model checkpoint saved at: {best_checkpoint_path}\n'
                 )
             print(
-                f'Lyla: Congratulations on completing the training run for the {model_name} model!'
-                f' Details are saved in {training_details}.'
+                f'Lyla: Congratulations on completing the training run for the {model_name} model! Details are saved in {training_details}.'
             )
 
         print('Lyla: It was a pleasure assisting you. Until next time!')
@@ -641,7 +711,8 @@ def main() -> None:
                 eval_period,
             )
 
+
 if __name__ == '__main__':
     main()
     if dist.is_initialized():
-        dist.destroy_process_group()
+        cleanup()

transformer/model.py
@@ -1,14 +1,20 @@
 # =============================================================================#
-# Authors: Isabel Liu, Windsor Nguyen
+# Authors: Windsor Nguyen, Isabel Liu
 # File: (Transformer) model.py
 #
-# Full definition of a GPT-based Transformer language model, adapted for regression.
+# Full definition of a standard Transformer model adapted for regression
+# on physics-based trajectories with
+#
+# 1). Flash Attention,
+# 2). Memory-Efficient Attention, and
+# 3). Dilated Attention
+#
 # References:
 # 1) the official GPT-2 TensorFlow implementation released by OpenAI:
 # https://github.com/openai/gpt-2/blob/master/src/model.py
+#
 # 2) huggingface/transformers PyTorch implementation:
 # https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
-#
 # =============================================================================#
 
 
@@ -17,13 +23,21 @@ import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from einops import rearrange
 from tqdm import tqdm
+from transformer.utils import padding_to_multiple_of, all_gather_func, get_data_parallel_rank, get_data_parallel_world_size
 from dataclasses import dataclass
 
 
 class CausalSelfAttention(nn.Module):
     """
     Self-attention layer for the Transformer.
+
+    Note: scaled_dot_product_attention enables FlashAttention-2
+    (Tri Dao, 2023, "FlashAttention-2: Faster Attention with Better Parallelism and Work Partitioning")
+    and Memory-Efficient Attention (Rabe et al., 2022, "Self-attention Does Not Need O(n^2) Memory"),
+    all written in C++, per the PyTorch documentation:
+    https://pytorch.org/docs/stable/generated/torch.nn.functional.scaled_dot_product_attention.html
     """
 
     def __init__(self, config):
@@ -45,7 +59,7 @@ class CausalSelfAttention(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
 
-        # Flash attention makes GPU go brrrrr but support is only in PyTorch >= 2.0
+        # Flash attention makes the GPUs go brrrrr, but support is only in PyTorch >= 2.0
         self.flash = hasattr(
             torch.nn.functional, 'scaled_dot_product_attention'
         )
@@ -77,6 +91,8 @@ class CausalSelfAttention(nn.Module):
 
         # Compute query, key, values for all heads in batch, and move head forward to be the batch dim
         q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+
+        # Reshape for multi-head attention
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(
             1, 2
         )  # (B, nh, T, hs)
@@ -107,7 +123,229 @@ class CausalSelfAttention(nn.Module):
             y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
 
         # Re-assemble all head outputs side by side
-        y = y.transpose(1, 2).contiguous().view(B, T, C)  # -> (B, 1, T, C)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        # Output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+
+
+class DilatedCausalSelfAttention(CausalSelfAttention):
+    """
+    Dilated causal self-attention layer, as implemented in the LongNet paper
+    (Ding et al., 2023, "LongNet: Scaling Transformers to 1,000,000,000 Tokens").
+
+    This code was adapted from torchscale/component/dilated_attention.py.
+    The repository can be found at https://github.com/microsoft/torchscale.
+    """
+
+    def dense_to_sparse(self, x, ratio):
+        # Get the length of the sequence
+        length = x.size(1)
+
+        # Calculate padding needed for sequence length and number of heads to be multiples of ratio
+        padding = (ratio - length % ratio) % ratio
+        head_padding = (ratio - self.n_head % ratio) % ratio
+
+        # Apply padding if needed
+        if padding > 0 or head_padding > 0:
+            x = F.pad(x, (0, 0, 0, head_padding, 0, padding), value=0.0)
+
+        # Rearrange tensor to apply dilated attention
+        x = rearrange(
+            x, 'b (l r1) (r2 h) d -> b l h d r1 r2', r1=ratio, r2=ratio
+        )
+        x = torch.diagonal(x, offset=0, dim1=4, dim2=5)
+        x = rearrange(x, 'b l h d r -> b l (r h) d')
+
+        # Remove extra padding from heads
+        if head_padding > 0:
+            x = x[:, :, : self.n_head]
+
+        return x
+
+    def sparse_to_dense(self, out, lse, ratio):
+        # Calculate padding needed for number of heads to be a multiple of ratio
+        head_padding = (ratio - self.n_head % ratio) % ratio
+
+        # Apply padding if needed
+        if head_padding > 0:
+            out = F.pad(out, (0, 0, 0, head_padding), value=0.0)
+            lse = F.pad(lse, (0, 0, 0, head_padding), value=-1e8)
+
+        # Rearrange tensor to convert back from sparse to dense representation
+        out = rearrange(out, 'b l (r h) d -> b l h d r', r=ratio)
+        out = torch.diag_embed(out, offset=0, dim1=4, dim2=5)
+        out = rearrange(
+            out, 'b l h d r1 r2 -> b (r2 h) (l r1) d', r1=ratio, r2=ratio
+        )
+
+        # Handle logsumexp for sparse to dense conversion
+        lse = rearrange(lse, 'b (r h) l -> b l h r', r=ratio)
+        lse = torch.diag_embed(lse, offset=0, dim1=3, dim2=4)
+        lse = lse.masked_fill_(lse == 0, -1e8)
+        lse = rearrange(
+            lse, 'b l h r1 r2 -> b (r2 h) (l r1) 1', r1=ratio, r2=ratio
+        )
+
+        # Remove extra padding from heads
+        if head_padding > 0:
+            out = out[:, : self.n_head]
+            lse = lse[:, : self.n_head]
+
+        return out, lse
+
+    def gather_kv(self, x, sl, seq_len, is_causal=True):
+        # Get batch size
+        bsz = x.size(0)
+
+        # Ensure segment length is a multiple of sequence length
+        assert sl % seq_len == 0
+        num_rank_per_segment = sl // seq_len
+
+        # Gather all key-value pairs from different ranks
+        x = all_gather_func(x)
+        current_rank = get_data_parallel_rank()
+        x = rearrange(x, '(w b) l h d -> w b l h d', b=bsz)
+
+        # Apply causal masking if needed
+        if is_causal:
+            if current_rank > 0:
+                x = x[:current_rank]
+            else:
+                x = x[:1] * 0
+
+        # Get current segment based on rank
+        current_segment = (
+            current_rank // num_rank_per_segment * num_rank_per_segment
+        )
+        x = x[current_segment : current_segment + num_rank_per_segment]
+
+        # Rearrange tensor to combine segments
+        x = rearrange(x, 'w b l h d -> b (w l) h d')
+        return x
+
+    def gathering(
+        self, x, dr, sl, is_causal=True, offset=0, is_kv=False, seq_parall=True
+    ):
+        curr_x = x
+
+        # Apply padding if offset is greater than zero
+        if offset > 0:
+            curr_x = F.pad(curr_x, (0, 0, 0, 0, offset % sl, 0), value=0.0)
+
+        # Get sequence length
+        seq_len = curr_x.size(1)
+
+        # Determine if key-value pairs should be gathered based on sequence parallelism
+        should_gather_kv = is_kv and seq_parall and (sl > seq_len)
+        _sl = sl
+        sl = min(sl, seq_len)
+        padding = (sl - seq_len % sl) % sl
+
+        # Apply padding if needed
+        if padding > 0:
+            curr_x = F.pad(curr_x, (0, 0, 0, 0, 0, padding), value=0.0)
+
+        # Rearrange tensor for dilated attention
+        curr_x = rearrange(curr_x, 'b (n g) h d -> (b n) g h d', g=sl)
+        curr_x = self.dense_to_sparse(curr_x, dr)
+
+        # Gather key-value pairs if needed
+        if should_gather_kv:
+            curr_x = self.gather_kv(curr_x, _sl, seq_len, is_causal)
+
+        # Rearrange tensor for attention computation
+        curr_x = rearrange(curr_x, 'b l h d -> (b h) l d')
+
+        return curr_x
+
+    # TODO: Initialize the dilation ratios to what the paper used.
+    def scattering(self, outs, lses, seq_len, bsz, offset=0):
+        all_outs, all_lses = [], []
+        drs = [1]  # TODO: (Dynamically) replace with actual dilation ratios
+        if len(outs) > len(drs):
+            drs = drs * (len(outs) // len(drs))
+
+        for dr, o, lse in zip(drs, outs, lses, strict=True):
+            o = rearrange(o, 'b l (h d) -> b l h d', h=self.n_head)
+            o, lse = self.sparse_to_dense(o, lse, dr)
+            o = rearrange(o, '(b n) h g d -> (b h) (n g) d', b=bsz)
+            lse = rearrange(lse, '(b n) h g 1 -> (b h) (n g) 1', b=bsz)
+            o = o[:, offset : offset + seq_len]
+            lse = lse[:, offset : offset + seq_len]
+            all_outs.append(o)
+            all_lses.append(lse)
+
+        with torch.no_grad():
+            max_lse = torch.stack(all_lses, dim=0).max(0)[0]
+            all_lses = [torch.exp(lse - max_lse) for lse in all_lses]
+            lse_sum = torch.stack(all_lses, dim=0).sum(0)
+            all_lses = [lse / lse_sum for lse in all_lses]
+
+        out = sum(
+            o * lse.type_as(o)
+            for o, lse in zip(all_outs, all_lses, strict=True)
+        )
+        out = rearrange(out, '(b h) l d -> b l (h d)', h=self.n_head)
+        return out
+
+    def forward(self, x):
+        # Get batch size, sequence length, and embedding dimension
+        B, T, C = x.size()
+
+        # Compute query, key, and value projections
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        # Initialize lists for storing outputs and logsumexp results
+        outs, lses = [], []
+
+        # Replace with actual segment lengths and dilation ratios
+        for sl, dr in zip([128], [1], strict=True):
+            # Gather key, value, and query tensors
+            ki = self.gathering(
+                k, dr, sl, is_causal=True, is_kv=True, seq_parall=True
+            )
+            vi = self.gathering(
+                v, dr, sl, is_causal=True, is_kv=True, seq_parall=True
+            )
+            qi = self.gathering(
+                q, dr, sl, is_causal=True, is_kv=False, seq_parall=True
+            )
+
+            if self.flash:
+                out, lse = torch.nn.functional.scaled_dot_product_attention(
+                    qi,
+                    ki,
+                    vi,
+                    attn_mask=None,
+                    dropout_p=self.dropout if self.training else 0,
+                    is_causal=True,
+                )
+            else:
+                att = (qi @ ki.transpose(-2, -1)) * (
+                    1.0 / math.sqrt(ki.size(-1))
+                )
+                att = att.masked_fill(
+                    self.mask[:, :, :sl, :sl] == 0, float('-inf')
+                )
+                att = F.softmax(att, dim=-1)
+                att = self.attn_dropout(att)
+                out = (
+                    att @ vi
+                )  # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, hs)
+
+            outs.append(out)
+            lses.append(lse)
+
+        # Scatter outputs and logsumexp results
+        y = self.scattering(outs, lses, T, B)
+
+        # Re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # -> (B, T, C)
 
         # Output projection
         y = self.resid_dropout(self.c_proj(y))
@@ -138,17 +376,23 @@ class MLP(nn.Module):
         return x
 
 
-class Block(nn.Module):
+class TransformerBlock(nn.Module):
     """
     Single block of the Transformer.
     """
 
     def __init__(self, config):
         super().__init__()
+        self.attn = self.get_attn_type(config)
         self.ln_1 = nn.LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd, bias=config.bias)
         self.mlp = MLP(config)
+
+    def get_attn_type(self, config):
+        if config.use_dilated_attn:
+            return DilatedCausalSelfAttention(config)
+        else:
+            return CausalSelfAttention(config)
 
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
@@ -166,6 +410,7 @@ class TransformerConfigs:
     max_len: int = 1_000
     bias: bool = False  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     dropout: float = 0.25
+    use_dilated_attn: bool = True
     loss_fn: nn.Module = None
 
 
@@ -183,7 +428,9 @@ class Transformer(nn.Module):
             dict(
                 wpe=nn.Embedding(config.max_len, config.n_embd),
                 dropout=nn.Dropout(config.dropout),
-                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                h=nn.ModuleList(
+                    [TransformerBlock(config) for _ in range(config.n_layer)]
+                ),
                 ln_f=nn.LayerNorm(config.n_embd, bias=config.bias),
             )
         )
@@ -252,12 +499,12 @@ class Transformer(nn.Module):
             x = block(x)
         x = self.transformer.ln_f(x)
         preds = self.regression_head(x)  # -> (batch_size, seq_len, d_out)
-
-        if targets is not None:
-            loss = self.loss_fn(preds, targets)
-        else:
-            loss = None
-        return preds, loss
+        return preds
+        # if targets is not None:
+        #     loss = self.loss_fn(preds, targets)
+        # else:
+        #     loss = None
+        # return preds, loss
 
     # TODO: Not sure when/where this could be used, but we'd like to use it!
     def estimate_mfu(self, fwdbwd_per_iter, dt):
@@ -337,7 +584,7 @@ class Transformer(nn.Module):
         # Initialize initial autoregressive sequences up to `init` steps for each trajectory
         ar_sequences[:, : init + 1, :] = inputs[:, : init + 1, :]
         u_start = targets.shape[2]
-        u_end = inputs.shape[2] # TODO: Unused...
+        u_end = inputs.shape[2]  # TODO: Unused...
 
         # Iterate over the specified number of time steps
         for i in tqdm(range(steps), desc='Predicting', unit='step'):